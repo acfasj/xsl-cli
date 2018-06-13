c8832ba4856510043f6cfaf42956d7212846988b
黄宏凯 <acfasj@gmail.com>
2018-06-13 11:15:26 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'extends': 'standard',
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0
+  }
+}
